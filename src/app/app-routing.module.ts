d6ef850d4a5f4fdef69e57cf172f0185bb6f6ce8
Kerollos Fessal <kerofessal77@gmail.com>
2024-03-13 21:40:48 +0200

    no hashing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -73,7 +73,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, {useHash:true, scrollPositionRestoration: 'enabled' }),
+    RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled' }),
   ],
   exports: [RouterModule],
 })
